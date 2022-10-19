2db382650d95b117113dd41ed31460f04eb32958
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2022-10-19 09:51:31 +0800

    Update and rename XrayEasyLoader_v1.0.ps1 to XrayEasyLoader.ps1

--- XrayEasyLoader.ps1
+++ XrayEasyLoader.ps1
@@ -1,11 +1,11 @@
-﻿$name = Read-Host '请输入目标地址(不带http://)'
+$name = Read-Host '请输入网站地址(不带http://)'
 $httpsq = "https://"
 $httpq = "http://"
 $ws = New-Object -ComObject WScript.Shell  
-$wsr = $ws.popup("目标网站是否有SSL证书（https://）？",0,"请选择",4 + 64)
+$wsr = $ws.popup("网站是否有SSL证书（https://）？",0,"请选择",4 + 64)
 if ($wsr -eq 6){$choose = $httpsq} else{$choose = $httpq}
 $urlname = $choose + $name + "/"
-./Xray webscan --basic-crawler $urlname --html-output out.html
+./Xray webscan --basic-crawler $urlname --html-output output.html
 $Ubakdate = "{0:yyyy}" -f (Get-Date)
 $Filename = $name + "_" + $Ubakdate + "年"
 $Ubakdate = "{0:MM}" -f (Get-Date)
@@ -17,4 +17,11 @@ $Filename = $Filename + $Ubakdate + "时"
 $Ubakdate = "{0:mm}" -f (Get-Date)
 $Filename = $Filename + $Ubakdate + "分.html"
 $End = $Filename -replace '[:]','_'
-rename-Item out.html -NewName $End
+if(Test-Path output.html){
+Write-Output "[+] 对于目标的扫描发现可利用点，正在导出报告并重命名"
+rename-Item output.html -NewName $End
+}
+Else{
+Write-Output "[-] 对于目标的扫描并未发现漏洞，无导出报告"
+cmd /c "pause"
+}
